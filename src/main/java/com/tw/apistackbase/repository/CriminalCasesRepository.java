e5e10bc2b67db2cf20d58cb6960485972c5afd27
VeuraGJ <yuanxiao_ru@163.com>
2019-07-19 19:47:31 +0800

    refactor:Story1

--- src/main/java/com/tw/apistackbase/repository/CriminalCasesRepository.java
+++ src/main/java/com/tw/apistackbase/repository/CriminalCasesRepository.java
@@ -1,8 +1,12 @@
 package com.tw.apistackbase.repository;
 
+
 import com.tw.apistackbase.entity.CriminalCase;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 
--- src/test/java/com/tw/apistackbase/repository/CriminalCasesRepositoryTest.java
+++ src/test/java/com/tw/apistackbase/repository/CriminalCasesRepositoryTest.java
@@ -1,6 +1,5 @@
 package com.tw.apistackbase.repository;
 
-import com.tw.apistackbase.entity.CaseDetail;
 import com.tw.apistackbase.entity.CriminalCase;
 import org.junit.Before;
 import org.junit.Test;
@@ -12,8 +11,10 @@ import org.springframework.data.domain.Sort;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertSame;
@@ -24,7 +25,10 @@ import static org.junit.Assert.assertSame;
 public class CriminalCasesRepositoryTest {
     @Autowired
     private CriminalCasesRepository criminalCasesRepository;
-
+//    @Autowired
+//    private CaseDetailRepository caseDetailRepository;
+//    @Autowired
+//    private ProcuratorateRepository procuratorateRepository;
     @Test
     public void should_add_criminal_case_when_give_vaild_criminal_case(){
         CriminalCase criminalCase = new CriminalCase();
@@ -36,21 +40,21 @@ public class CriminalCasesRepositoryTest {
     @Test
     public void should_not_add_criminal_case_when_give_unvaild_criminal_case(){
         CriminalCase criminalCase = new CriminalCase();
-        CriminalCase criminalCase1 =criminalCasesRepository.save(criminalCase);
-        assertSame(null,criminalCase1.getCaseName());
+        Assertions.assertThrows(Exception.class,()->{
+            criminalCasesRepository.saveAndFlush(criminalCase);
+        });
     }
     @Test
     public void should_return_specific_criminal_cases_when_query_function(){
-        CriminalCase criminalCase = criminalCasesRepository.findById(Long.valueOf(1)).orElse(null);
+        CriminalCase criminalCase = criminalCasesRepository.findById(1L).orElse(null);
         assertSame("BBBB",criminalCase.getCaseName());
         assertEquals(123143223,criminalCase.getIncidentTime());
     }
     @Test
     public void should_return_all_criminal_cases_when_query_function(){
         List<CriminalCase> criminalCases = criminalCasesRepository.findAll(new Sort(Sort.Direction.DESC,"incidentTime"));
-        CriminalCase queryCriminalCase = criminalCases.stream().findFirst().orElse(null);
-        assertSame("CCCC",queryCriminalCase.getCaseName());
-        assertEquals(764832224,queryCriminalCase.getIncidentTime());
+        List<CriminalCase> sortedCriminalCases=criminalCases.stream().sorted(Comparator.comparingLong(CriminalCase::getIncidentTime).reversed()).collect(Collectors.toList());
+        assertEquals(sortedCriminalCases,criminalCases);
     }
     @Test
     public void should_return_all_criminal_cases_when_find_by_caseName_function(){
@@ -59,22 +63,29 @@ public class CriminalCasesRepositoryTest {
     }
     @Test
     public void should_return_delete_criminal_case_when_delete_function(){
-        criminalCasesRepository.deleteById(Long.valueOf(1));
+        criminalCasesRepository.deleteById(1L);
         List<CriminalCase> criminalCases = criminalCasesRepository.findAll();
         assertEquals(2,criminalCases.size());
     }
-    @Test
-    public void should_specific_criminalCase_with_caseDetail_when_query_function(){
-        CriminalCase criminalCase = criminalCasesRepository.findById(Long.valueOf(1)).orElse(null);
-        Assertions.assertEquals("IHIUGIBUKB",criminalCase.getCaseDetail().getObjectiveDescription());
-        Assertions.assertEquals("DHOUHUIUYG",criminalCase.getCaseDetail().getSubjectiveDescription());
-    }
+//    @Test
+//    public void should_specific_criminalCase_with_caseDetail_when_query_function(){
+//        CriminalCase criminalCase = criminalCasesRepository.findById(Long.valueOf(1)).orElse(null);
+//        Assertions.assertEquals("IHIUGIBUKB",criminalCase.getCaseDetail().getObjectiveDescription());
+//        Assertions.assertEquals("DHOUHUIUYG",criminalCase.getCaseDetail().getSubjectiveDescription());
+//    }
+//    @Test
+//    public void should_specific_criminalCase_with_procuratorate_when_query_function(){
+//        CriminalCase criminalCase = criminalCasesRepository.findById(Long.valueOf(1)).orElse(null);
+//        Assertions.assertEquals("LLLL",criminalCase.getProcuratorate().getName());
+//    }
     @Before
     public void setUp() throws Exception {
         List<CriminalCase> criminalCases = new ArrayList<>();
-        criminalCases.add(new CriminalCase("BBBB",123143223,new CaseDetail("DHOUHUIUYG","IHIUGIBUKB")));
-        criminalCases.add(new CriminalCase("BBBB",123143224,new CaseDetail("DHOUHUYG","IHIUGIBUKB")));
-        criminalCases.add(new CriminalCase("CCCC",764832224,new CaseDetail("YG","IUKB")));
+        criminalCases.add(new CriminalCase("BBBB",123143223));
+        criminalCases.add(new CriminalCase("BBBB",123143224));
+        criminalCases.add(new CriminalCase("CCCC",764832224));
+//        criminalCases.add(new CriminalCase("DDDD",640839224,new Procuratorate("UUUU")));
         criminalCasesRepository.saveAll(criminalCases);
+//        caseDetailRepository.save(new CaseDetail("YGY","IUKB"));
     }
 }
